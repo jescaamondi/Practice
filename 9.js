d4cb03853ae33a751a97cdf407a9607879312bcd
JescaAmondi <oduorjescaamondi@gmail.com>
2026-04-28 20:15:38 +0300

    final commit

--- 9.js
+++ 9.js
@@ -1,9 +1,13 @@
 function findCheck(arr, target){
-    for(let i=0;i<arr.length; i++)
+    for(let i=0;i<arr.length; i++){
         if (target===arr[i]){
             console.log("Found it!");
-        
+            return;
+         }
+         }
+console.log("not Found!");
+    
         }
 
-}
-findCheck([1,2,3,4,5,6],5);
+findCheck([1,2,3,4,5,6], 5);
+
